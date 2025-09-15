88af5a44721cb2d162fe9c5a6035a02e991089c8
zeles <rojomodmini3@gmail.com>
2025-09-15 17:10:48 +0500

    build was deleted from repository

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_NoteSync_FILE D:/Work/NoteSync/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/NoteSync.exe)
-set(__QT_DEPLOY_TARGET_NoteSync_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_NoteSync_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
